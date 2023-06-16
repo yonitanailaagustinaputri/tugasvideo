dc9ca606472beb5c5e737fe39cb9b45c55b02ee3
yonitanailaagustinaputri <124542668+yonitanailaagustinaputri@users.noreply.github.com>
2023-06-16 08:34:52 +0700

    Add files via upload

---
+++ edit_barang.php
@@ -0,0 +1,48 @@
+<div class="row">
+    <h2>
+    Edit Barang
+    </h2>
+    <hr>
+    <div class="col-lg-4">
+    <div class="panel panel-primary">
+        <div class="panel-heading">Edit Barang Inventaris</div>
+            <div class="panel-body">     
+        <form action="">
+            <div class="form-group">
+                <label for="">Kode Barang</label>
+                <input type="text" class="form-control" placehoder="Masukan Nama Barang">
+            </div>
+            <div class="form-group">
+                <label for="">Nama Barang</label>
+                <input type="text" class="form-control" placehoder="Masukan Nama Barang">
+            </div>
+            <div class="form-group">
+                <label for="" >Kondisi</label>
+                <select name="" id="" class="from-control">
+                    <option value="" name="" class="from-control">- Pilih -</option>
+                    <option value="" name="" class="from-control">Baik</option>
+                    <option value="" name="" class="from-control">Rusak</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="">Jumlah</label>
+                <input type="number" class="form-control" placehoder="Masukan Jumlah Barang">
+            </div>
+            <div class="form-group">
+                <label for="">Nama Ruang</label>
+                <input type="text" class="form-control" placehoder="Masukan Nama Ruang">
+            </div>
+            <div class="form-group">
+                <label for="">Keterangan</label>
+                 <textarea name="" id="" cols="20" rows="5" class="form-control"></textarea>
+            </div>
+            <div class="form-group">
+               <button class="btn btn-md btn-primary">Simpan</button>
+               <a href="?p=list_barang" class="btn btn-md btn-default">Kembali</a>
+            </div>
+        </form>
+              </div>
+            </div>
+        </div>
+    
+</div>
---
+++ list_barang.php
@@ -0,0 +1,67 @@
+<h2><center>DAFTAR INVENTARIS</center></h2>
+<hr>
+<a href="?p=tambah_barang" class="btn btn-md btn-primary"><span class="glyphicon glyphicon-plus"></span></a>
+<form class="navbar-form navbar-right" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Cari Barang">
+        </div>
+        <button type="submit" class="btn btn-default">Cari</button>
+      </form>
+<br><br>
+<table class="table table-borderet">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Kode Inventaris</th>
+            <th>Nama Barang</th>
+            <th>Kondisi</th>
+            <th>Jumlah</th>
+            <th>Ruang</th>
+            <th>Tanggal Register</th>
+            <th>Keterangan</th>
+            <th>Opsi</th>
+        </tr>
+    </thead>
+    <body>
+        <tr>
+            <td>1</td>
+            <td>L001</td>
+            <td>Laptop</td>
+            <td>Baik</td>
+            <td>50</td>
+            <td>LAB RPL</td>
+            <td>19-11-2021</td>
+            <td>Barang di dapat dari bantuan provinsi</td>
+            <td>
+                <a href="?p=edit_barang"class="btn-md btn-primary"><span class="glyphicon glyphicon-edit"></span></a>
+                <a href=""class="btn-md btn-danger"><span class="glyphicon glyphicon-trash"></span></a>
+            </td>
+        </tr>
+    </body>
+</table>
+
+<div>
+
+</div>
+
+<div class="" style="float:right">
+<nav>
+  <ul class="pagination">
+    <li>
+      <a href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+    <li><a href="#">1</a></li>
+    <li><a href="#">2</a></li>
+    <li><a href="#">3</a></li>
+    <li><a href="#">4</a></li>
+    <li><a href="#">5</a></li>
+    <li>
+      <a href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
+</nav>
+</div>
